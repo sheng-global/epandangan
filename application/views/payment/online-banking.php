691a150b12653c580dae6f8c3195d70d7458ddbf
PAAB Administrator <no-reply@paab.my>
2020-02-11 00:12:54 +0800

    Add migs logo
    Signed-off-by: PAAB Administrator <no-reply@paab.my>

--- application/views/payment/online-banking.php
+++ application/views/payment/online-banking.php
@@ -7,29 +7,30 @@
                             <div class="card-body p-4">
                                 
                                 <div class="text-center w-75 m-auto">
-                                    <img src="<?php echo BASE_URL."assets/images/fpx.png" ?>">
-                                    <p class="text-muted mb-4 mt-3" data-tag="payment"></p>
+                                    <p class="text-muted mb-4 mt-3">Pembayaran</p>
                                 </div>
 
                                 <form method="post" role="form" action="<?php echo BASE_URL ?>payment/online_process" id="reload-payment" class="form-horizontal">
                                     <div class="form-group mb-3">
-                                        <label for="payment_type" class="control-label">Payment for</label>
+                                        <label for="payment_type" class="control-label">Bayaran untuk</label>
                                         <input name="payment_type" class="form-control select2" readonly required="" value="Draf PSKL2040">
                                     </div>
                                     <div class="form-group mb-3">
-                                        <label for="amount" class="control-label">Amount</label>
-                                        <div class="input-group">
-                                            <span class="input-group-addon">RM</span>
-                                            <input type="number" id="amount" name="amount" class="form-control" value="1.00" required="">
-                                        </div>
+                                        <label for="amount" class="control-label">Jumlah (RM)</label>
+                                        <input type="number" id="amount" name="amount" class="form-control" value="1.00" required="">
                                     </div>
                                     
                                     <div class="form-group mb-3">
-                                        <label for="bank_code" class="control-label">Payment Mode</label>
+                                        <label for="bank_code" class="control-label">Kaedah Pembayaran</label>
                                         <div class="radio radio-inline">
                                             <img src="<?php echo BASE_URL."assets/images/fpx.png" ?>">
                                             <input type="radio" id="fpx" value="fpx" name="payment_mode" required="" checked="checked">
-                                            <label for="fpx">Internet Banking (Individual)</label>
+                                            <label for="fpx">Perbankan Internet (Individu)</label>
+                                        </div>
+                                        <div class="radio radio-inline">
+                                            <img src="<?php echo BASE_URL."assets/images/migs.jpeg" ?>">
+                                            <input type="radio" id="migs" value="migs" name="payment_mode">
+                                            <label for="migs">Kad Kredit</label>
                                         </div>
                                     </div>
 
@@ -38,21 +39,19 @@
                                         <input type="text" class="form-control" readonly="" value="fadlisaad@gmail.com">
                                     </div>
 
-                                    <div class="form-group mb-3">
-                                        <div id="select_bank"></div>
-                                    </div>
+                                    <div id="select_bank"></div>
 
                                     <div class="form-group">
-                                        <label for="tnc" class="control-label">Terms &amp; Condition</label>
+                                        <label for="tnc" class="control-label">Terma &amp; Syarat</label>
                                         <div class="text-mute">
-                                            By clicking Proceed below, you are agree to FPX's <a href="https://www.mepsfpx.com.my/FPXMain/termsAndConditions.jsp" target="_new">terms and conditions</a>
+                                            Dengan ini saya bersetuju dengan <a href="https://www.mepsfpx.com.my/FPXMain/termsAndConditions.jsp" target="_new">Terma dan Syarat</a> FPX.
                                         </div>
                                     </div>
 
                                     <input type="hidden" name="be_message" value="<?php echo $be_message ?>">
 
                                     <div class="form-group mb-0 text-center">
-                                        <button class="btn btn-info" type="submit" name="submit" data-tag="login"></button>
+                                        <button class="btn btn-info" type="submit" name="submit">Bayar</button>
                                     </div>
                                 </form>
 
